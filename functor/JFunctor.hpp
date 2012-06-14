5bf1641a5f7a0e19481569ce7feea57dc783eba3
Jason Wong <jason.hcwong@gmail.com>
2012-06-14 15:44:26 +0800

    functor: support natural syntax

--- functor/JFunctor.hpp
+++ functor/JFunctor.hpp
@@ -13,6 +13,19 @@
 #define MY_PP_8(L)  MY_PP_7(L)  L(8)
 #define MY_PP_9(L)  MY_PP_8(L)  L(9)
 
+// you will see: typename P1, typename P2 ......
+#define TEMPLATE_ARG(L) typename P##L
+#define TEMPLATE_ARG_0		       typename R
+#define TEMPLATE_ARG_1 TEMPLATE_ARG_0, TEMPLATE_ARG(1)
+#define TEMPLATE_ARG_2 TEMPLATE_ARG_1, TEMPLATE_ARG(2)
+#define TEMPLATE_ARG_3 TEMPLATE_ARG_2, TEMPLATE_ARG(3)
+#define TEMPLATE_ARG_4 TEMPLATE_ARG_3, TEMPLATE_ARG(4)
+#define TEMPLATE_ARG_5 TEMPLATE_ARG_4, TEMPLATE_ARG(5)
+#define TEMPLATE_ARG_6 TEMPLATE_ARG_5, TEMPLATE_ARG(6)
+#define TEMPLATE_ARG_7 TEMPLATE_ARG_6, TEMPLATE_ARG(7)
+#define TEMPLATE_ARG_8 TEMPLATE_ARG_7, TEMPLATE_ARG(8)
+#define TEMPLATE_ARG_9 TEMPLATE_ARG_8, TEMPLATE_ARG(9)
+
 // you will see: P1 p1, P2 p2, P3 p3 ...... 
 #define FUNCTION_ARG(L) P##L p##L
 #define FUNCTION_ARG_0
@@ -25,6 +38,19 @@
 #define FUNCTION_ARG_7 FUNCTION_ARG_6, FUNCTION_ARG(7)
 #define FUNCTION_ARG_8 FUNCTION_ARG_7, FUNCTION_ARG(8)
 #define FUNCTION_ARG_9 FUNCTION_ARG_8, FUNCTION_ARG(9)
+
+// you will see: P1, P2, P3 ...... 
+#define FUNCTION_ARG2(L) P##L
+#define FUNCTION_ARG2_0
+#define FUNCTION_ARG2_1                  FUNCTION_ARG2(1)
+#define FUNCTION_ARG2_2 FUNCTION_ARG2_1, FUNCTION_ARG2(2)
+#define FUNCTION_ARG2_3 FUNCTION_ARG2_2, FUNCTION_ARG2(3)
+#define FUNCTION_ARG2_4 FUNCTION_ARG2_3, FUNCTION_ARG2(4)
+#define FUNCTION_ARG2_5 FUNCTION_ARG2_4, FUNCTION_ARG2(5)
+#define FUNCTION_ARG2_6 FUNCTION_ARG2_5, FUNCTION_ARG2(6)
+#define FUNCTION_ARG2_7 FUNCTION_ARG2_6, FUNCTION_ARG2(7)
+#define FUNCTION_ARG2_8 FUNCTION_ARG2_7, FUNCTION_ARG2(8)
+#define FUNCTION_ARG2_9 FUNCTION_ARG2_8, FUNCTION_ARG2(9)
 
 // you will see: p1, p2, p3...... 
 #define FUNCTION_PARAM(L) p##L
@@ -40,8 +66,47 @@
 #define FUNCTION_PARAM_9 FUNCTION_PARAM_8, FUNCTION_PARAM(9)
 
 
-class NullClass{};
+class NullClass;
 
+template<typename signature>
+class functorN;
+
+#define NaturalSyntax(L) \
+template<TEMPLATE_ARG_##L> \
+class functorN< R (FUNCTION_ARG2_##L) > \
+{ \
+private: \
+  typedef R (*functor_fp_##L)(FUNCTION_ARG_##L); \
+  typedef R (NullClass::*memFun_##L)(FUNCTION_ARG_##L); \
+public: \
+  functorN(functor_fp_##L a) \
+  { \
+    isMemFun = false; \
+    memcpy((void *) &m_fp, (void *) &a, sizeof(functor_fp_##L)); \
+  } \
+  template<typename X, typename Y> \
+  functorN(X *x, R (Y::*a)(FUNCTION_ARG_##L)) \
+  { \
+    isMemFun = true; \
+    m_pthis = x; \
+    memcpy((void *) &m_memFun, (void *) &a, sizeof(memFun_##L)); \
+  } \
+  R operator () (FUNCTION_ARG_##L) \
+  { \
+    if (isMemFun) \
+      return (((NullClass *) (m_pthis))->*(m_memFun))(FUNCTION_PARAM_##L); \
+    else \
+      return (m_fp)(FUNCTION_PARAM_##L); \
+  } \
+private: \
+  functor_fp_##L   m_fp; \
+  memFun_##L       m_memFun; \
+  void             *m_pthis; \
+  bool             isMemFun; \
+};
+
+MY_PP_9(NaturalSyntax)
+/******************************************************************************/
 
 template<typename ret_type = void, typename P1 = NullClass, 
          typename P2 = NullClass,  typename P3 = NullClass, 
--- functor/main.cpp
+++ functor/main.cpp
@@ -56,7 +56,6 @@ double test1(double a)
   return a;
 }
 
-
 void performanceTest(int n = 500000000)
 {
   struct timeval startTimeU, endTimeU;
@@ -65,6 +64,30 @@ void performanceTest(int n = 500000000)
 
   clock_gettime(CLOCK_REALTIME, &startTimeN);
   functor<int, int, int> h(&b, &A::d2);
+  clock_gettime(CLOCK_REALTIME, &endTimeN);
+  printf("time for creating a functor: %ld ns\n", (endTimeN.tv_sec - startTimeN.tv_sec) * 1000000000 + (endTimeN.tv_nsec - startTimeN.tv_nsec));
+
+  clock_gettime(CLOCK_REALTIME, &startTimeN);
+  h(19, 20);
+  clock_gettime(CLOCK_REALTIME, &endTimeN);
+  printf("time for calling functor: %ld ns\n", (endTimeN.tv_sec - startTimeN.tv_sec) * 1000000000 + (endTimeN.tv_nsec - startTimeN.tv_nsec));
+
+  gettimeofday(&startTimeU, NULL);
+  for (int counter = 0; counter < n; ++counter) {
+    h(19, 20);
+  }
+  gettimeofday(&endTimeU, NULL);
+  printf("time for calling functor for %d times: %ld us\n", n, (endTimeU.tv_sec - startTimeU.tv_sec) * 1000000 + (endTimeU.tv_usec - startTimeU.tv_usec));
+}
+
+void performanceTestN(int n = 500000000)
+{
+  struct timeval startTimeU, endTimeU;
+  struct timespec startTimeN, endTimeN;
+  A b;
+
+  clock_gettime(CLOCK_REALTIME, &startTimeN);
+  functorN<int (int, int)> h(&b, &A::d2);
   clock_gettime(CLOCK_REALTIME, &endTimeN);
   printf("time for creating a functor: %ld ns\n", (endTimeN.tv_sec - startTimeN.tv_sec) * 1000000000 + (endTimeN.tv_nsec - startTimeN.tv_nsec));
 
@@ -94,15 +117,36 @@ int main(void)
   A a;
   A b;
   A c;
-  functor<void, int> g(&a, &A::d);
-  functor<void, int> i(&A::static_b);
-  functor<int, int, int, int, int, int, int, int, int, int> j(&c, &A::d9);
+  functor<void, int> f2(&a, &A::d);
+  functor<void, int> f3(&A::static_b);
+  functor<int, int, int, int, int, int, int, int, int, int> f4(&c, &A::d9);
 
-  g(17);
-  i(21);
-  assert(j(1,2,3,4,5,6,7,8,9) == (1+2+3+4+5+6+7+8+9));
+  f2(17);
+  f3(21);
+  assert(f4(1,2,3,4,5,6,7,8,9) == (1+2+3+4+5+6+7+8+9));
 
-  performanceTest(1000000);
+/***************************** Natrual Syntax ********************************/
+  // test ordinary function
+  functorN<void( ) > g0(&test0);
+  g0();
+
+  functorN<double (double)> g1(&test1);
+  assert(12.123 == g1(12.123));
+
+  // test member function
+  functorN<void (int)> g2(&a, &A::d);
+  functorN<void (int)> g3(&A::static_b);
+  functorN<int (int, int, int, int, int, int, int, int, int)> g4(&c, &A::d9);
+
+  g2(17);
+  g3(21);
+  assert(g4(1,2,3,4,5,6,7,8,9) == (1+2+3+4+5+6+7+8+9));
+
+
+
+/* performance test */
+  performanceTest (500000000);
+  performanceTestN(500000000);
 
   return 0;
 }
